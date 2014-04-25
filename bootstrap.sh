f94381e1672d3aefbbdca295a062a86c49a998d0
Vincent Petithory <vincent.petithory@gmail.com>
2014-04-25 18:05:04 -0500

    Fixed bootstrap install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # VM has self contained updater & build bot
-sudo cp /vagrant/tmwa-init /etc/init.d/tmwa-init
-sudo chown vagrant:vagrant /etc/init.d/tmwa-init
+sudo install -m755 -o vagrant -g vagrant -t /etc/init.d /vagrant/tmwa-init
 /etc/init.d/tmwa-init install
