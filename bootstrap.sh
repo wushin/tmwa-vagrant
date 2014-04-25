cb7aef9e7a0d6d7c9c3d753b683b60d2b35a1354
wushin <pasekei@gmail.com>
2014-04-25 16:15:15 -0500

    Fixed copy perms for init script additions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # VM has self contained updater & build bot
-cp /vagrant/tmwa-init /etc/init.d/tmwa-init
+sudo cp /vagrant/tmwa-init /etc/init.d/tmwa-init
+sudo chown vagrant:vagrant /etc/init.d/tmwa-init
 /etc/init.d/tmwa-init install
